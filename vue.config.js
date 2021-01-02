2e2883a7281035cbed4a95ad6587c05ad582835a
dukker <865116277@qq.com>
2021-01-02 11:30:18 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,25 @@
 module.exports = {
   chainWebpack(config) {
-    config.module
-      .rule('tsx')
-      .use('ts-loader')
-      .loader('ts-loader')
-      .tap((opts) => {
-        opts.transpileOnly = false
-        opts.happyPackMode = false
-        return opts
-      })
-    config.module
-      .rule('ts')
-      .use('ts-loader')
-      .loader('ts-loader')
-      .tap((opts) => {
-        opts.transpileOnly = false
-        opts.happyPackMode = false
-        return opts
-      })
+    if (process.env.NODE_ENV === 'production') {
+      config.module
+        .rule('tsx')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap((opts) => {
+          opts.transpileOnly = false
+          opts.happyPackMode = false
+          return opts
+        })
+      config.module
+        .rule('ts')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap((opts) => {
+          opts.transpileOnly = false
+          opts.happyPackMode = false
+          return opts
+        })
+    }
   },
   // productionSourceMap: false,
   parallel: false,
