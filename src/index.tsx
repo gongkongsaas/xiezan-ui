f8d1fd1cffe42ed6204d3511f11b600687d6813d
dukker <865116277@qq.com>
2021-01-02 11:00:47 +0800

    添加配置文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,2 @@
-// export { default as Button } from './components/Button';
 export { default as Button } from './components/Button'
 export { default as Layout } from './components/Layout'
-export * from './components/Input'
